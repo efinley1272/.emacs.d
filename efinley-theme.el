6e62424abec8458f9eb57f47b53d164df7c1e77d
Elliot Finley <efinley@efinley.com>
2019-02-05 12:03:16 -0700

    first commit

---
+++ efinley-theme.el
@@ -0,0 +1,13 @@
+(deftheme efinley
+  "Created 2019-01-21.")
+
+(custom-theme-set-variables
+ 'efinley
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (alchemist))))
+
+(custom-theme-set-faces
+ 'efinley
+ '(highlight ((t (:foreground "white" :background "dark green")))))
+
+(provide-theme 'efinley)
