69a34b1c5589cb63441dcb2856f831616c1424d2
efinley <efinley@efinley.com>
2020-01-03 00:49:55 +0000

    added yaml mode

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (alchemist))))
+ '(package-selected-packages (quote (yaml-mode alchemist))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
